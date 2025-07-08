1c850683898d3a916e1defba19810c4492dd403d
Max Meriberg <merive.official@gmail.com>
2025-07-08 15:26:01 +0300

    Добавлены заголовки конструкторов в DateTime.h

--- include/DateTime.h
+++ include/DateTime.h
@@ -3,7 +3,13 @@
 
 class DateTime {
 private:
-long long seconds;
+	long long seconds;
+
+public:
+	DateTime();
+	DateTime(long long seconds);
+	DateTime(int year, int month, int day);
+	DateTime(DateTime& datetime);
 };
 
 #endif // DATETIME_H_
