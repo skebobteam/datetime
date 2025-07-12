7091c5a888725807f074699be97527326b2e87a8
PripravaOstraya <linnikovaiv05@gmail.com>
2025-07-12 17:39:03 +0300

    Добавляет меню в main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,111 @@
+#include "DateTime.h"
 #include <iostream>
 
-int main() {
-	return 0;
+int main()
+{
+    bool continuation = true;
+    int num_case;
+    std::string separator = "\n-------------------------------------------------\n";
+    std::cout << separator;
+    std::cout << "\nMenu:\n   0: End program.\n   1: Create object (seconds).\n   2: Create object (year, month, day).\n   3: Create object (year, month, day, hour, minute, second).\n   4: Create object (string).\n   5: Compare two objects.\n   6: Add days.\n   7: Add months.\n   8: Add years.\n   9: Get day of the week.\n   10: Time now.\n   11: Print.\n";
+    std::cout << separator;
+
+    DateTime sampler;
+
+    while (continuation) {
+        std::cout << "\n\nEnter the case number: ";
+        std::cin >> num_case;
+        switch (num_case) {
+
+        case 1: {
+            int seconds;
+            std::cout << "Enter the number (seconds): ";
+            std::cin >> seconds;
+            DateTime tmp(seconds);
+            sampler = tmp;
+            break;
+        }
+
+        case 2: {
+            int year, month, day;
+            std::cout << "Enter the numbers (year, month, day): ";
+            std::cin >> year >> month >> day;
+            DateTime tmp(year, month, day);
+            sampler = tmp;
+            break;
+        }
+
+        case 3: {
+            int year, month, day, hour, minute, second;
+            std::cout << "Enter the numbers (year, month, day, hour, minute, second): ";
+            std::cin >> year >> month >> day >> hour >> minute >> second;
+            DateTime tmp(year, month, day, hour, minute, second);
+            sampler = tmp;
+            break;
+        }
+
+        case 4: {
+            std::string str;
+            std::cout << "Enter the string (DD.MM.YY hh:mm:ss): ";
+            std::cin.ignore();
+            getline(std::cin, str);
+            DateTime tmp(str);
+            sampler = tmp;
+            break;
+        }
+
+        case 5: {
+            int seconds;
+            std::cout << "Enter the second object (seconds) to compare with sampler: ";
+            std::cin >> seconds;
+            DateTime tmp(seconds);
+            std::cout << sampler.Compare(tmp, sampler);
+            break;
+        }
+
+        case 6: {
+            int days;
+            std::cout << "Enter the number of days: ";
+            std::cin >> days;
+            sampler.AddDays(days);
+            break;
+        }
+
+        case 7: {
+            int months;
+            std::cout << "Enter the number of months: ";
+            std::cin >> months;
+            sampler.AddMonths(months);
+            break;
+        }
+
+        case 8: {
+            int years;
+            std::cout << "Enter the number of years: ";
+            std::cin >> years;
+            sampler.AddYears(years);
+            break;
+        }
+
+        case 9: {
+            std::cout << sampler.GetWeekDay();
+            break;
+        }
+
+        case 10: {
+            sampler = sampler.GetNow();
+            break;
+        }
+
+        case 11: {
+            std::cout << sampler.ToString();
+            break;
+        }
+
+        case 0: {
+            continuation = false;
+            break;
+        }
+        }
+    }
 }
